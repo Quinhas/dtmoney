3568f264e1eb89862cb0a0fcc6fa05c773e14f27
Quinhas <ln.santana08@gmail.com>
2021-08-16 19:00:22 -0300

    Contextos e hooks

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -4,7 +4,7 @@ import outcomeImg from "../../assets/outcome.svg";
 import closeImg from "../../assets/close.svg";
 import { Container, RadioBox, TransactionTypeContainer } from "./styles";
 import { FormEvent, useState } from "react";
-import { api } from "../../services/api";
+import { useTransactions } from "../../hooks/useTransactions";
 
 type NewTransactionModalProps = {
   isOpen: boolean;
@@ -16,21 +16,26 @@ export function NewTransactionModal({
   onRequestClose,
 }: NewTransactionModalProps) {
   const [title, setTitle] = useState("");
-  const [value, setValue] = useState(0);
+  const [amount, setAmount] = useState(0);
   const [category, setCategory] = useState("");
   const [type, setType] = useState("deposit");
+  const { createTransaction } = useTransactions();
 
-  function handleCreateNewTransaction(event: FormEvent) {
+  async function handleCreateNewTransaction(event: FormEvent) {
     event.preventDefault();
 
-    const data = {
+    await createTransaction({
       title,
-      value,
+      amount,
       category,
       type,
-    };
+    });
 
-    api.post("/transactions", data);
+    setTitle("");
+    setAmount(0);
+    setCategory("");
+    setType("deposit");
+    onRequestClose();
   }
 
   return (
@@ -53,13 +58,13 @@ export function NewTransactionModal({
           type="text"
           placeholder="Título"
           value={title}
-          onChange={(event) => setValue(Number(event.target.value))}
+          onChange={(event) => setTitle(event.target.value)}
         />
         <input
           type="number"
           placeholder="Valor"
-          value={value}
-          onChange={(event) => setTitle(event.target.value)}
+          value={amount}
+          onChange={(event) => setAmount(Number(event.target.value))}
         />
 
         <TransactionTypeContainer>
@@ -84,7 +89,7 @@ export function NewTransactionModal({
         </TransactionTypeContainer>
 
         <input
-          type="number"
+          type="text"
           placeholder="Categoria"
           value={category}
           onChange={(event) => setCategory(event.target.value)}
